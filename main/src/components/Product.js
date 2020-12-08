b529fb9eb7a15e1fb652ef41605c47fce37dd143
Stefan Codruta <stefancodruta18@gmail.com>
2020-12-08 14:40:42 +0200

    donish

--- main/src/components/Product.js
+++ main/src/components/Product.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react'
 
 class Product extends Component {
+    render() {
+        return (
+            <div>
+                Id: {this.props.id} |
+                Name: {this.props.name} |
+                Price: {this.props.price} 
+                <input type="button" value="buy" onClick={() => this.props.onBuy(this.props.price)}/>
+            
+            </div>
+        )
+    }
+    
 }
 
 export default Product
--- main/src/components/VendingMachine.js
+++ main/src/components/VendingMachine.js
@@ -11,21 +11,48 @@ class VendingMachine extends Component {
         }
 
         this.addToken = () => {
-            
+
+            let no = this.state.tokens + 1;
+            this.setState({
+                tokens: no
+            });
         }
 
         this.buyProduct = (price) => {
-           
+           if (this.state.tokens >= price) {
+               console.log('Bought.');
+               let tokensRemained = this.state.tokens - price;
+               this.setState({
+                   tokens: tokensRemained
+               })
+           } else {
+               console.log('not enough tokens');
+           }
         }
+
     }
 
+
+    componentDidMount() {
+        let ps = new ProductStore();
+        let prods = ps.getAll();
+
+        // for (let item of prods) {
+        this.setState({
+            products: Array.from(prods)
+        })
+    // }
+    
+
+        this.state.products.map((el, index) => console.log(index, el));
+    }
 
     render() {
         return (
             <div>
-                {this.state.products.map((el, index) => <Product key={index} name={el.name} price={el.price} onBuy={this.buyProduct}  />)}
+                {this.state.products.map((el, index) => <Product key={index} id={el.id} name={el.name} price={el.price} onBuy={this.buyProduct}  />)}
                 <div>Tokens: {this.state.tokens}</div>
-                <input type="button" value="add token" />
+                <input type="button" value="add token" onClick={this.addToken}/>
             </div>
         )
     }
